f76fef6090e7acf95ebf44c9e3224288beead8b7
Jomo Kigo <kigojay684@gmail.com>
2023-09-18 13:45:14 +0300

    modified:   Colapsing Sidebar/src/index.css 	modified:   Colapsing Sidebar/src/index.css.map 	modified:   Colapsing Sidebar/src/index.html 	modified:   Colapsing Sidebar/src/index.js 	modified:   Colapsing Sidebar/src/index.scss

--- Colapsing Sidebar/src/index.js
+++ Colapsing Sidebar/src/index.js
@@ -1,20 +1,28 @@
 const collapse = document.getElementById("collapse");
 const sidebar = document.getElementById("sidebar");
 const desc = document.querySelectorAll(".desc");
+const collapseIcon = document.getElementById("collapse-icon");
+const main = document.getElementById("main");
 var click = 0;
 
 collapse.addEventListener("click", () => {
-    if (click === 0) {
-        desc.forEach((element) => {
-          element.style.display = "none";
-        });
-        sidebar.style.width = "75px";
-        click = 1;
-    } else {
-        desc.forEach((element) => {
-          element.style.display = "block";
-        });
-        sidebar.style.width = "200px";
-        click = 0;
-    }
+  if (click === 0) {
+    desc.forEach((element) => {
+      element.style.display = "none";
+    });
+    sidebar.style.width = "75px";
+    collapseIcon.style.transform = "rotate(180deg)";
+    collapseIcon.style.transition = "all .5s ease";
+    main.style.width = "calc(100% - 75px)";
+    click = 1;
+  } else {
+    desc.forEach((element) => {
+      element.style.display = "block";
+    });
+    sidebar.style.width = "200px";
+    collapseIcon.style.transform = "rotate(0deg)";
+    collapseIcon.style.transition = "all .5s ease";
+    main.style.width = "calc(100% - 200px)";
+    click = 0;
+  }
 });
